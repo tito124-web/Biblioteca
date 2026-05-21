dc9d86670e2acd42021dd971760ac816beb8def7
Tito <jmartinezo12@miumg.edu.gt>
2026-05-20 22:47:40 -0600

    feat: Agregando gestor de Material

--- src/Material/Book.java
+++ src/Material/Book.java
@@ -13,10 +13,22 @@ public class Book extends Material{
 
 	@Override
 	public String toString() {
-		
-		return super.toString();
+		return "Book [code=" + code + ", year=" + year + ", title=" + title + ", author=" + author + ", available="
+				+ available + "]";
 	}
 	
 	
+	public String toArchivo() {
+	    return "BOOK;" + code + ";" + year + ";" + title + ";" + author + ";" + available;
+	}
+	
+	// Reconstruye el objeto desde una línea del archivo
+    public static Book fromArchivo(String linea) {
+        String[] p = linea.split(";");
+        Book b = new Book(p[1], p[2], p[3], p[4]);
+        b.setDisponible(Boolean.parseBoolean(p[5]));
+        return b;
+    }
+
 
 }
--- src/Material/Magazine.java
+++ src/Material/Magazine.java
@@ -11,12 +11,25 @@ public class Magazine extends Material {
 		return diasRetraso * 1.50;
 	}
 
+	
+	
 	@Override
 	public String toString() {
-		
-		return super.toString();
+		return "Magazine [code=" + code + ", year=" + year + ", title=" + title + ", author=" + author + ", available="
+				+ available + "]";
 	}
-
 	
+	  // Convierte el objeto a texto para guardar
+    public String toArchivo() {
+        return "MAGAZINE;" + code + ";" + year + ";" + title + ";" + author + ";" + available;
+    }
+
+	 // Reconstruye el objeto desde una línea del archivo
+    public static Magazine fromArchivo(String linea) {
+        String[] p = linea.split(";");
+        Magazine m = new Magazine(p[1], p[2], p[3], p[4]);
+        m.setDisponible(Boolean.parseBoolean(p[5]));
+        return m;
+    }
 	
 }
