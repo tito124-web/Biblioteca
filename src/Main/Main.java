2d1dd0284627b8962b8fe8a7be57d34bad047aa7
Tito <jmartinezo12@miumg.edu.gt>
2026-05-26 20:53:01 -0600

    feat: Instanciando jFrame en main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,25 @@
 package Main;
 
+
+
+import ui.MainFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		 
+		        new MainFrame().setVisible(true);
+		    
+		
+		
+		
+		
+	}
+		public static void test() {
 		System.out.println("Felipe Sandoval");
 		System.out.println("Gabriela Medrano");
-
-	}
+		}
+	
 
 }
