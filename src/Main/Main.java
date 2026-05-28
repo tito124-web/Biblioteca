370ac0b1d566535747dedbd3eb55de391dab1e23
Felipesandx <jsandovalr9@miumg.edu.gt>
2026-05-28 09:08:38 -0600

    chore: limpiando metodo test en el main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,14 +12,6 @@ public class Main {
 		        new MainFrame().setVisible(true);
 		    
 		
-		
-		
-		
 	}
-		public static void test() {
-		System.out.println("Felipe Sandoval");
-		System.out.println("Gabriela Medrano");
-		}
-	
 
 }
