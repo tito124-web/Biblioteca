de3cd4baaa0a6948f25a538fd0f946ef69e9f972
Omarexd <acabreraj@miumg.edu.gt>
2026-05-28 12:46:21 -0600

    update: cambios en gestor de bitacora

--- src/Bitacora/GestorBitacora.java
+++ src/Bitacora/GestorBitacora.java
@@ -1,5 +1,30 @@
 package Bitacora;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class GestorBitacora {
 
+    private static final String ARCHIVO = "bitacora.csv";
+
+    // Registrar acciones del sistema
+    public static void registrar(String accion, String detalle) {
+
+        try (BufferedWriter bw =
+                new BufferedWriter(new FileWriter(ARCHIVO, true))) {
+
+            bw.write(LocalDateTime.now()
+                    + ";" + accion
+                    + ";" + detalle);
+
+            bw.newLine();
+
+        } catch (IOException e) {
+
+            System.out.println("Error al guardar bitácora.");
+
+        }
+    }
 }
